3b7cc352567156db7efb4348785683b9727789e6
umeeee0325 <nana19970204@icloud.com>
2024-01-11 18:38:25 +0900

    Revert "Merge pull request #8 from umeeee0325/second_training"
    This reverts commit 5fbd6643fb799d6f1b238e8e49085fb500aa0251, reversing
    changes made to b6dc7eb9b2fda65ac0a7a3747d439e296211e6c8.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_03_133754) do
-
   create_table "plans", charset: "utf8mb4", force: :cascade do |t|
     t.string "plan"
     t.date "date"
